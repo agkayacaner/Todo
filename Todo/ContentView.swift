19bc5672f0044c9c66749004073cfd04f41ca865
agkayacaner <dev.caneragkaya@gmail.com>
2024-03-01 20:53:00 +0300

    isCompleted is now with animation effect

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
                                 
                             Text(todo.title)
                                 .strikethrough(todo.isCompleted)
+                                .animation(.bouncy)
                         }
                         .foregroundColor(todo.isCompleted ? .secondary : .primary)
                     }
