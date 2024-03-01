b60e2f0fa6a95a009091df10fdc16857382acdf3
agkayacaner <dev.caneragkaya@gmail.com>
2024-03-01 19:31:39 +0300

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Caner Ağkaya on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
